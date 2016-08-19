3f4d7d5a6e4bfed5c2c4963a977d713464dbc87b
Garay,Jerry <jerry.garay@gmail.com>
2016-08-19 11:58:41 -0400

    Initial Commit

---
+++ DrawpboxTests/DrawpboxTests.swift
@@ -0,0 +1,36 @@
+//
+//  DrawpboxTests.swift
+//  DrawpboxTests
+//
+//  Created by Garay,Jerry on 8/19/16.
+//  Copyright © 2016 Garay,Jerry. All rights reserved.
+//
+
+import XCTest
+@testable import Drawpbox
+
+class DrawpboxTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
